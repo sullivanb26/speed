35261b93a75485720856b611142f0b3531c35ddb
sullivanb26 <sullivanb26@wpsma.org>
2024-01-26 18:24:14 -0500

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,15 +1,15 @@
 /// hello.js
 addEventListener("DOMContentLoaded", (event) => { 
   var btn = document.createElement("BUTTON");
-  btn.innerHTML = "Click me!";
-  btn.style.backgroundColor = "red";
+  btn.innerHTML = "Run!";
+  btn.style.backgroundColor = "purple";
+  btn.style.color = "white";
   btn.style.position = "absolute";
   btn.style.left = "10px";
-  btn.style.bottom = "10px;
+  btn.style.bottom = "10px";
   btn.onclick = function(){
     let run = prompt("Enter text");
-    alert('clicked');
+    eval(run);
   };
   document.body.appendChild(btn);
-  alert("button made!");
 });
